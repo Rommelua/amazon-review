e3835d8f6451a058cb3c0af7adc2d9fa6c7f31e2
Leonid <rommel@ukr.net>
2020-11-15 17:26:52 +0200

    Initial commit

---
+++ src/test/java/com/amazon/review/AmazonReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
